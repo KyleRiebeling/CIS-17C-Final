074aea4b4085337f5e4bad35261db0b9bdc45d67
KyleRiebeling <125304972+KyleRiebeling@users.noreply.github.com>
2023-06-09 15:48:54 -0700

    Finished problem 4

--- Problem 4/MergeVSSelection/main.cpp
+++ Problem 4/MergeVSSelection/main.cpp
@@ -41,54 +41,84 @@ void selection(int *arr, int size, int stop) {
    }
 }
 
+void merge(int a[], int left, int mid, int right) {
+   oper+=2;
+   int span = right - left;
+   oper+=2;
+   int count1 = left, countH = mid;
+   oper++;
+   int *c = new int[span];
+
+   oper++;
+   for (int i = 0; i < span; i++) {
+      oper+=2;
+      oper++;
+      if (count1 == mid) {
+         oper+=3;
+         c[i] = a[countH++];
+         oper++;
+      } else if (countH == right) {
+         oper+=3;
+         c[i] = a[count1++];
+         oper+=3;
+      } else if (a[count1] < a[countH]) {
+         oper+=3;
+         c[i] = a[count1++];
+      } else {
+         oper+=3;
+         c[i] = a[countH++];
+      }
+   }
+
+   oper++;
+   for (int i = 0; i < span; i++) {
+      oper+=2;
+      oper+=3;
+      a[left + i] = c[i];
+   }
+
+   delete []c;
+}
+
+void mergeSort(int a[], int begin, int end) {
+   oper+=2;
+   int mid = (begin + end) / 2;
+   oper+=2;
+   if ((mid - begin) > 1)mergeSort(a, begin, mid);
+   oper+=2;
+   if ((end - mid) > 1)mergeSort(a, mid, end);
+   merge(a, begin, mid, end);
+}
+
 int main(int argc, char** argv) {
    srand(time(0));
    const int AMOUNT = 100000;
-   const int SORT_AMOUNT = 100000;
+   const int SORT_AMOUNT = 20;
 
    int *array = new int[AMOUNT];
-   int *copy = new int[AMOUNT];
+   int copy[AMOUNT]; //for merge sort
 
    for (int i = 0; i < AMOUNT; i++) {
       array[i] = rand() % 1000;
       copy[i] = array[i];
    }
 
-
-
    auto start = high_resolution_clock::now();
-
    //Do Merge
-
+   mergeSort(copy, 0, AMOUNT);
    auto stop = high_resolution_clock::now();
    auto duration = duration_cast<microseconds>(stop - start);
-
    cout << "Merge took " << duration.count() << " microseconds with " << oper << " operations" << endl;
 
-   //Reset array so that both sorts face the same array
-   for (int i = 0; i < AMOUNT; i++) {
-      copy[i] = array[i];
-   }
-
    oper = 0;
+   
    start = high_resolution_clock::now();
-
    //Do Selection
-   selection(copy, AMOUNT, SORT_AMOUNT);
-
+   selection(array, AMOUNT, SORT_AMOUNT);
    stop = high_resolution_clock::now();
    duration = duration_cast<microseconds>(stop - start);
-
-   cout << "Selection took " << duration.count() << " microseconds with " << oper << " operations" << endl;
-
-   for (int i = 1; i <= AMOUNT; i++) {
-      if (i < SORT_AMOUNT + 50) {
-         cout << copy[i-1] << " ";
-         if (i % 10 == 0)cout << endl;
-      }
-   }
-   cout << endl;
-
+   cout << "Selection with " << SORT_AMOUNT << " elements took " << duration.count() << " microseconds with " << oper << " operations" << endl;
+   
    return 0;
 }
 
